2309d1cd35fb357b30f2bcf0658e2e8818aa738c
ethan-lo <114978477+ethan-lo@users.noreply.github.com>
2022-10-15 22:45:38 +0800

    added led wall

--- main.ts
+++ main.ts
@@ -8,6 +8,42 @@ radio.onReceivedValue(function (name, value) {
     if (name == "stop") {
         stop = value
     }
+    if (name == "heart") {
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+    }
+    if (name == "smile") {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    }
+    if (name == "diamond") {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+    }
+    if (name == "circle") {
+        basic.showLeds(`
+            . # # # .
+            # . . . #
+            # . . . #
+            # . . . #
+            . # # # .
+            `)
+    }
 })
 let distance = 0
 let stop = 0
@@ -17,7 +53,7 @@ music.playMelody("C D E F G - - - ", 120)
 radio.setGroup(7)
 basic.forever(function () {
     distance = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
-    if (distance < 10 && stop == 0) {
+    if (distance < 30 && stop == 0) {
         cuteBot.stopcar()
         stop = 1
         music.playMelody("C5 B C5 B C5 B C5 B ", 200)
