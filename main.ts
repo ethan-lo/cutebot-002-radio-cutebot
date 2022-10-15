e4cb3bed79a54a6d978cdcd29b6f0bc728838e2a
ethan-lo <114978477+ethan-lo@users.noreply.github.com>
2022-10-15 22:33:38 +0800

    Added Stop function

--- main.ts
+++ main.ts
@@ -5,11 +5,22 @@ radio.onReceivedValue(function (name, value) {
     if (name == "y") {
         yValue = value
     }
+    if (name == "stop") {
+        stop = value
+    }
 })
+let distance = 0
+let stop = 0
 let yValue = 0
 let xValue = 0
 music.playMelody("C D E F G - - - ", 120)
 radio.setGroup(7)
 basic.forever(function () {
+    distance = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
+    if (distance < 10 && stop == 0) {
+        cuteBot.stopcar()
+        stop = 1
+        music.playMelody("C5 B C5 B C5 B C5 B ", 200)
+    }
     cuteBot.motors(yValue + xValue, yValue - xValue)
 })
